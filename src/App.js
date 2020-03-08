86861a2ce3772cc818f025e3146252ebe88d794d
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-03-08 19:43:23 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // Reactはどこでも使われていない。
 // JSXはトランスコンパイルされる場合に、Reactが使用される。
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10},
-    { name: "Hanako", age: 10},
-    { name: "NoAge"}
+    { name: "Hanako", age: 5},
+    { name: "NoName", age: 2}
   ];
   return (
     <div>
@@ -24,8 +25,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
