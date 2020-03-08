3687f3f8be2bc1181e28eb415a970ba872423d47
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-03-08 17:38:07 +0900

    patice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 class App extends Component {
+  //why?
+    /**
+     * したのようにトランスパイルされる。
+     * JSXの方が可読性が高い。
+     */
   render() {
-    return <div><h1>Hello.World!</h1></div>;
+    //これがJSX (JS XML) JSの拡張言語
+    return <h1>Hello.World!</h1>;
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello,World!!!"
+//     );
+//   }
+// }
 
 export default App;
